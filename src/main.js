171c2cabd6a7b3f79d82d7c9cb3ce8f464fcdb39
SimoneCappadona <simocappa95@gmail.com>
2024-02-19 11:35:40 +0100

    import components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Bootstrap
+import 'bootstrap';
+// FontAwesomeIcon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.component("search-bar", SearchBar);
+app.component("filter-bar", FilterBar);
 
 createApp(App).mount('#app')
