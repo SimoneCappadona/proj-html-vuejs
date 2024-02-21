685484b4fa340cb65a313fff59fe32b8bbb82b6d
SimoneCappadona <simocappa95@gmail.com>
2024-02-21 14:48:58 +0100

    add slider

---
+++ src/components/appMember.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      Social: [
+        "fa-brands fa-facebook-f",
+        "fa-brands fa-twitter",
+        "fa-brands fa-instagram",
+      ],
+    };
+  },
+  props: { card: Array },
+};
+</script>
+<template>
+  <div class="container">
+    <div class="row g-3 flex-nowrap text-center">
+      <div class="col-4 mt-5" v-for="card in card" :key="card">
+        <img :src="card.imageurl" alt="" class="mx-auto" />
+        <p class="surname fs-3 fw-bolder p-1">{{ card.name }}</p>
+        <p class="jobs fw-medium-">{{ card.job }}</p>
+        <div class="d-flex gap-2 mb-5 justify-content-center">
+          <div class="social" v-for="socialicon in Social" :key="socialicon">
+            <font-awesome-icon :icon="socialicon" class="icons" />
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/components/appTeam.vue
+++ src/components/appTeam.vue
@@ -1,68 +1,79 @@
 <script>
+import appMember from "./appMember.vue";
 export default {
   data() {
     return {
+      cardIndex: 1,
       cards: [
-        {
-          imageurl: "src/assets/img/team1.png",
-          name: "Robert Coleman",
-          job: "Owner & Creative Director",
-        },
-        {
-          imageurl: "src/assets/img/team2.png",
-          name: "Don Woods",
-          job: "Administrator",
-        },
-        {
-          imageurl: "src/assets/img/team4.png",
-          name: "Tomas Nash",
-          job: "2d Animator & Compositor",
-        },
-        {
-          imageurl: "src/assets/img/team1.png",
-          name: "Barbon Cino",
-          job: "Pet Sitter",
-        },
-        {
-          imageurl: "src/assets/img/team4.png",
-          name: "Coca Cola",
-          job: "Master on Food Machines",
-        },
-        {
-          imageurl: "src/assets/img/team2.png",
-          name: "Olio Cuore",
-          job: "Ambassador of Oil",
-        },
-        {
-          imageurl: "src/assets/img/team4.png",
-          name: "Pape Rino",
-          job: "C.E.O",
-        },
-        {
-          imageurl: "src/assets/img/team2.png",
-          name: "Burro Cacao",
-          job: "Vendor",
-        },
-        {
-          imageurl: "src/assets/img/team1.png",
-          name: "Pinco Pallo",
-          job: "Master on Dots",
-        },
-        {
-          imageurl: "src/assets/img/team2.png",
-          name: "Topo Lino",
-          job: "Co-C.E.O",
-        },
-        {
-          imageurl: "src/assets/img/team1.png",
-          name: "Pluto Wuf",
-          job: "Wuf Wuf",
-        },
-        {
-          imageurl: "src/assets/img/team4.png",
-          name: "Qui Quo Qua",
-          job: "Scout",
-        },
+        ,
+        [
+          {
+            imageurl: "src/assets/img/team1.png",
+            name: "Robert Coleman",
+            job: "Owner & Creative Director",
+          },
+          {
+            imageurl: "src/assets/img/team2.png",
+            name: "Don Woods",
+            job: "Administrator",
+          },
+          {
+            imageurl: "src/assets/img/team4.png",
+            name: "Tomas Nash",
+            job: "2d Animator & Compositor",
+          },
+        ],
+        [
+          {
+            imageurl: "src/assets/img/team1.png",
+            name: "Barbon Cino",
+            job: "Pet Sitter",
+          },
+          {
+            imageurl: "src/assets/img/team4.png",
+            name: "Coca Cola",
+            job: "Master on Food Machines",
+          },
+          {
+            imageurl: "src/assets/img/team2.png",
+            name: "Olio Cuore",
+            job: "Ambassador of Oil",
+          },
+        ],
+        [
+          {
+            imageurl: "src/assets/img/team4.png",
+            name: "Pape Rino",
+            job: "C.E.O",
+          },
+          {
+            imageurl: "src/assets/img/team2.png",
+            name: "Burro Cacao",
+            job: "Vendor",
+          },
+          {
+            imageurl: "src/assets/img/team1.png",
+            name: "Pinco Pallo",
+            job: "Master on Dots",
+          },
+        ],
+        [
+          {
+            imageurl: "src/assets/img/team2.png",
+            name: "Topo Lino",
+            job: "Co-C.E.O",
+          },
+          {
+            imageurl: "src/assets/img/team1.png",
+            name: "Pluto Wuf",
+            job: "Wuf Wuf",
+          },
+          {
+            imageurl: "src/assets/img/team4.png",
+            name: "Qui Quo Qua",
+            job: "Scout",
+          },
+        ],
       ],
       Social: [
         "fa-brands fa-facebook-f",
@@ -71,21 +82,40 @@ export default {
       ],
     };
   },
+  methods: {
+    nextCard() {
+      this.cardIndex++;
+      if (this.cardIndex > this.cards.length - 1) {
+        this.cardIndex = 1;
+      }
+    },
+    prevCard() {
+      this.cardIndex--;
+      if (this.cardIndex < 1) {
+        this.cardIndex = this.cards.length - 1;
+      }
+    },
+  },
+  components: { appMember },
 };
 </script>
 <template>
   <div class="container">
-    <div class="row g-3 flex-nowrap">
-      <div class="col-4 mt-5" v-for="card in cards" :key="card">
-        <img :src="card.imageurl" alt="" />
-        <p class="surname fs-3 fw-bolder p-1">{{ card.name }}</p>
-        <p class="jobs fw-medium-">{{ card.job }}</p>
-        <div class="d-flex gap-2 mb-5">
-          <div class="social" v-for="socialicon in Social" :key="socialicon">
-            <font-awesome-icon :icon="socialicon" class="icons" />
-          </div>
-        </div>
+    <appMember :card="cards[cardIndex]" />
+    <div class="d-flex gap-3 justify-content-center">
+      <button class="btn btn-outline-secondary rounded-5" @click="prevCard()">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+      <div
+        class="dots mt-1"
+        v-for="i in 4"
+        
+      >
+        <i class="fa-solid fa-circle fa-xs" :class="i == cardIndex ? 'active' : ''"></i>
       </div>
+      <button class="btn btn-outline-secondary rounded-5" @click="nextCard()">
+        <i class="fa-solid fa-arrow-right"></i>
+      </button>
     </div>
   </div>
 </template>
@@ -103,5 +133,12 @@ export default {
   height: 50px;
   aspect-ratio: 1;
   cursor: pointer;
+}
+.dots {
+  cursor: pointer;
+  color: $purplegrey;
+}
+.active {
+  color: $lightorange;
 }
 </style>
